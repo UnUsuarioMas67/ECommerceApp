28a8f3d24e89af7d869ee75a2aff2c28fdc65b9a
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-01-27 21:14:51 -0400

    Add AdminConfig.cs

---
+++ ECommerce.Api/Infrastructure/EF/Configuration/AdminConfig.cs
@@ -0,0 +1,38 @@
+﻿using ECommerce.Api.Domain.Entities;
+using ECommerce.Api.Domain.Validation;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ECommerce.Api.Infrastructure.EF.Configuration;
+
+public class AdminConfig : IEntityTypeConfiguration<Admin>
+{
+    public void Configure(EntityTypeBuilder<Admin> builder)
+    {
+        builder.Property(x => x.FirstName)
+            .IsRequired()
+            .HasMaxLength(TextLengths.Name);
+        
+        builder.Property(x => x.LastName)
+            .IsRequired()
+            .HasMaxLength(TextLengths.Name);
+        
+        builder.Property(x => x.Email)
+            .IsRequired()
+            .HasMaxLength(TextLengths.Email);
+        
+        builder.Property(x => x.PasswordHash)
+            .IsRequired()
+            .HasMaxLength(TextLengths.PasswordHash);
+        
+        builder.Property(x => x.PhoneNumber)
+            .IsRequired()
+            .HasMaxLength(TextLengths.PhoneNumber);
+
+        builder.Property(x => x.BirthDate)
+            .IsRequired();
+        
+        builder.Property(x => x.CreatedAt)
+            .IsRequired();
+    }
+}
