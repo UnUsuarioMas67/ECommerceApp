dcf6a268fd63616c936a62bbc19c19e6002a521a
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-01-27 20:04:16 -0400

    Add Admin entity

--- ECommerce.Api/Domain/Entities/Admin.cs
+++ ECommerce.Api/Domain/Entities/Admin.cs
@@ -1,20 +1,13 @@
 ﻿namespace ECommerce.Api.Domain.Entities;
 
-public class Client : IUser
+public class Admin : IUser
 {
     public int Id { get; set; }
-    
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Email { get; set; }
     public string PasswordHash { get; set; }
     public string PhoneNumber { get; set; }
     public DateOnly BirthDate { get; set; }
-    
     public DateTime CreatedAt { get; set; }
-    
-    public ICollection<ShopOrder> Orders { get; set; }
-    public ICollection<Cart> Carts { get; set; }
-    public ICollection<Address> Addresses { get; set; }
-    public ICollection<ClientReview> Reviews { get; set; }
 }
