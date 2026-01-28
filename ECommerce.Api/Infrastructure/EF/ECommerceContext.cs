e2e74424184d59d2ea8d88f7540ba7e7599648b5
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-01-28 01:05:52 -0400

    Seed Clients and Admins

--- ECommerce.Api/Infrastructure/EF/ECommerceContext.cs
+++ ECommerce.Api/Infrastructure/EF/ECommerceContext.cs
@@ -1,4 +1,5 @@
 ﻿using ECommerce.Api.Domain.Entities;
+using ECommerce.Api.Utils;
 using Microsoft.EntityFrameworkCore;
 
 namespace ECommerce.Api.Infrastructure.EF;
@@ -6,29 +7,75 @@ namespace ECommerce.Api.Infrastructure.EF;
 public class ECommerceContext : DbContext
 {
     public DbSet<Admin> Admins { get; set; }
-    
+
     public DbSet<Client> Clients { get; set; }
     public DbSet<ClientReview> ClientReviews { get; set; }
-    
+
     public DbSet<Product> Products { get; set; }
     public DbSet<Category> Categories { get; set; }
-    
+
     public DbSet<Address> Addresses { get; set; }
     public DbSet<Country> Countries { get; set; }
-    
+
     public DbSet<Cart> Carts { get; set; }
     public DbSet<CartItem> CartItems { get; set; }
-    
+
     public DbSet<ShopOrder> ShopOrders { get; set; }
     public DbSet<OrderStatus> OrderStatuses { get; set; }
     public DbSet<OrderLine> OrderLines { get; set; }
-    
-    public ECommerceContext() { }
-    
-    public ECommerceContext(DbContextOptions options) : base(options) { }
+
+    public ECommerceContext()
+    {
+    }
+
+    public ECommerceContext(DbContextOptions options) : base(options)
+    {
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ECommerceContext).Assembly);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSeeding((context, _) =>
+        {
+            var client = context.Set<Client>()
+                .FirstOrDefault(c => c.Email == "rp456@julio.com");
+            if (client == null)
+            {
+                var newClient = new Client
+                {
+                    FirstName = "Ramón",
+                    LastName = "Pruebas",
+                    Email = "rp456@julio.com",
+                    PhoneNumber = "849-666-7777",
+                    PasswordHash = PasswordHasher.HashPassword("cliente123"),
+                    BirthDate = new DateOnly(2005, 4, 13),
+                    CreatedAt = DateTime.Parse("2026-01-26"),
+                };
+                context.Set<Client>().Add(newClient);
+            }
+
+            var admin = context.Set<Admin>()
+                .FirstOrDefault(a => a.Email == "jp123@julio.com");
+            if (admin != null)
+            {
+                var newAdmin = new Admin
+                {
+                    FirstName = "Julio",
+                    LastName = "Pruebas",
+                    Email = "jp123@julio.com",
+                    PhoneNumber = "809-111-2222",
+                    PasswordHash = PasswordHasher.HashPassword("admin123"),
+                    BirthDate = new DateOnly(2001, 7, 22),
+                    CreatedAt = DateTime.Parse("2026-01-26"),
+                };
+                context.Set<Admin>().Add(newAdmin);
+            }
+            
+            context.SaveChanges();
+        });
     }
 }
---
+++ ECommerce.Api/Utils/PasswordHasher.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerce.Api.Utils;
+
+public static class PasswordHasher
+{
+    public static string HashPassword(string password)
+    {
+        var passwordHash = BCrypt.Net.BCrypt.HashPassword(password);
+        return passwordHash;
+    }
+}
