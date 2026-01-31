5d377b268b96521b3b8bc7f25b18ec1883d3371a
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-01-31 03:02:39 -0400

    Add DTO classes

---
+++ ECommerce.Api/Application/DTOs/AuthenticationDto.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ECommerce.Api.Application.DTOs;
+
+public class AuthenticationDto
+{
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public string? Message { get; set; }
+    
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public UserDto? User { get; set; }
+    
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public string? Token { get; set; }
+}
