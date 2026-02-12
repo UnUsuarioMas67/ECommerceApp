56589052605895d04a2d57a2e8e6655c63e79655
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-02-12 02:55:37 -0400

    Add AddressValidator and CategoryValidator

---
+++ ECommerce.Api/Application/Validators/AddressValidator.cs
@@ -0,0 +1,35 @@
+﻿using ECommerce.Api.Domain.Entities;
+using ECommerce.Api.Domain.Validation;
+using ECommerce.Api.Infrastructure.EF;
+using FluentValidation;
+
+namespace ECommerce.Api.Application.Validators;
+
+public class AddressValidator : AbstractValidator<Address>
+{
+    public AddressValidator(ECommerceContext context)
+    {
+        RuleFor(a => a.AddressLine1)
+            .NotEmpty()
+            .MaximumLength(TextLengthRules.ShortText);
+
+        RuleFor(a => a.AddressLine2)
+            .MaximumLength(TextLengthRules.ShortText);
+
+        RuleFor(a => a.City)
+            .NotEmpty()
+            .MaximumLength(TextLengthRules.ShortText);
+
+        RuleFor(a => a.PostalCode)
+            .NotEmpty()
+            .MaximumLength(TextLengthRules.PostalCode);
+
+        RuleFor(a => a.Region)
+            .MaximumLength(TextLengthRules.ShortText);
+        
+        var countries = context.Countries;
+        RuleFor(a => a.CountryId)
+            .NotEmpty()
+            .Must(id => countries.Any(c => c.Id == id));
+    }
+}
---
+++ ECommerce.Api/Application/Validators/CategoryValidator.cs
@@ -0,0 +1,30 @@
+﻿using ECommerce.Api.Domain.Entities;
+using ECommerce.Api.Domain.Validation;
+using ECommerce.Api.Infrastructure.EF;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Api.Application.Validators;
+
+public class CategoryValidator : AbstractValidator<Category>
+{
+    public CategoryValidator(ECommerceContext context)
+    {
+        RuleFor(c => c.Name)
+            .NotEmpty()
+            .MaximumLength(TextLengthRules.Name);
+
+        RuleFor(c => c.Description)
+            .NotEmpty()
+            .MaximumLength(TextLengthRules.ShortText);
+
+        var categories = context.Categories;
+        RuleFor(c => c.Name)
+            .MustAsync(async (category, name, token) =>
+            {
+                return !await categories.AnyAsync(c 
+                    => c.Name == name && c.Id != category.Id);
+            })
+            .WithMessage("Category with that name already exists.");
+    }
+}
