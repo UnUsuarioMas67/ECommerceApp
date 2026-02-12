7267464d78237141e4a97b29c71015b1ef1d5807
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-02-12 18:49:25 -0400

    Add CanParseIntoDate method

--- ECommerce.Api/Extensions/RuleBuilderOptionsExtensions.cs
+++ ECommerce.Api/Extensions/RuleBuilderOptionsExtensions.cs
@@ -14,13 +14,13 @@ public static class RuleBuilderOptionsExtensions
             .MustAsync(async (email, token) => !await dbSet.AnyAsync(u => u.Email == email))
             .WithMessage("Email address already in use");
     }
-    
+
     public static IRuleBuilderOptions<IUser, string> PhoneNumberNotAlreadyExists<T>(
         this IRuleBuilder<IUser, string> ruleBuilder,
         DbSet<T> dbSet) where T : class, IUser
     {
         return ruleBuilder
-            .MustAsync(async (phoneNumber, token) 
+            .MustAsync(async (phoneNumber, token)
                 => !await dbSet.AnyAsync(u => u.PhoneNumber == phoneNumber))
             .WithMessage("Phone number already in use");
     }
@@ -32,12 +32,19 @@ public static class RuleBuilderOptionsExtensions
             .Must((date) => date <= DateTime.Today)
             .WithMessage("Must not be in the future");
     }
-    
+
     public static IRuleBuilderOptions<T, DateOnly> NotInTheFuture<T>(this IRuleBuilder<T, DateOnly> ruleBuilder)
         where T : class
     {
         return ruleBuilder
             .Must(date => date <= DateOnly.FromDateTime(DateTime.Today))
             .WithMessage("Must not be in the future");
+    }
+
+    public static IRuleBuilder<T, string> CanParseIntoDate<T>(this IRuleBuilder<T, string> ruleBuilder)
+    {
+        return ruleBuilder
+            .Must(date => DateOnly.TryParse(date, out _))
+            .WithMessage("Not a valid date");
     }
 }
