6a1e9c696258d4b1e16b9f3d874ed2f523d277df
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-02-12 02:52:22 -0400

    Update ClientValidator to UserValidator
    - This converts ClientValidator into UserValidator
    - Moves UserValidator into /Application/Validators directory
    - Refactors validators into extension methods

---
+++ ECommerce.Api/Application/Validators/UserValidator.cs
@@ -0,0 +1,52 @@
+﻿using ECommerce.Api.Domain.Entities;
+using ECommerce.Api.Domain.Validation;
+using ECommerce.Api.Extensions;
+using ECommerce.Api.Infrastructure.EF;
+using FluentValidation;
+
+namespace ECommerce.Api.Application.Validators;
+
+public class UserValidator<T> : AbstractValidator<IUser> where T : class, IUser
+{
+    public UserValidator(ECommerceContext db)
+    {
+        var users = db.Set<T>();
+        
+        RuleFor(u => u.FirstName)
+            .NotEmpty()
+            .MaximumLength(TextLengthRules.Name);
+
+        RuleFor(u => u.LastName)
+            .NotEmpty()
+            .MaximumLength(TextLengthRules.Name);
+
+        RuleFor(u => u.Email)
+            .NotEmpty()
+            .MaximumLength(TextLengthRules.Email);
+
+        RuleFor(u => u.PhoneNumber)
+            .NotEmpty()
+            .MaximumLength(TextLengthRules.PhoneNumber);
+
+        RuleFor(u => u.BirthDate)
+            .NotNull()
+            .NotInTheFuture();
+
+        RuleFor(u => u.CreatedAt)
+            .NotNull()
+            .NotInTheFuture();
+
+        RuleFor(u => u.PasswordHash)
+            .NotEmpty();
+
+        RuleFor(u => u.Email)
+            .NotEmpty()
+            .MaximumLength(TextLengthRules.Email)
+            .EmailNotAlreadyExists(users);
+
+        RuleFor(u => u.PhoneNumber)
+            .NotEmpty()
+            .MaximumLength(TextLengthRules.PhoneNumber)
+            .PhoneNumberNotAlreadyExists(users);
+    }
+}
