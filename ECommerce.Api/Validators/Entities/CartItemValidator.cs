534c47bdb3f01cbc59c76f66793c1921828e7686
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-02-12 22:07:48 -0400

    Add CartValidator.cs and CartItemValidator.cs

---
+++ ECommerce.Api/Validators/Entities/CartItemValidator.cs
@@ -0,0 +1,27 @@
+﻿using ECommerce.Api.Domain.Entities;
+using ECommerce.Api.Infrastructure.EF;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Api.Validators.Entities;
+
+public class CartItemValidator : AbstractValidator<CartItem>
+{
+    public CartItemValidator()
+    {
+        RuleFor(ci => ci.Product)
+            .NotNull()
+            .WithName("ProductId")
+            .WithMessage("Product not specified or does not exist");
+
+        RuleFor(ci => ci.Cart)
+            .NotNull()
+            .WithName("CartId")
+            .WithMessage("Cart not specified or does not exist");
+        
+        RuleFor(ci => ci.Quantity)
+            .NotNull()
+            .GreaterThan(0)
+            .WithMessage("Quantity cannot be negative");
+    }
+}
---
+++ ECommerce.Api/Validators/Entities/CartValidator.cs
@@ -0,0 +1,28 @@
+﻿using ECommerce.Api.Domain.Entities;
+using ECommerce.Api.Infrastructure.EF;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Api.Validators.Entities;
+
+public class CartValidator : AbstractValidator<Cart>
+{
+    private readonly ECommerceContext _context;
+
+    public CartValidator(ECommerceContext context)
+    {
+        _context = context;
+
+        RuleFor(c => c.ClientId)
+            .NotNull()
+            .MustAsync(ClientExists)
+            .WithMessage("Client does not exist");
+        
+        RuleForEach(c => c.Items).SetValidator(new CartItemValidator());
+    }
+
+    private async Task<bool> ClientExists(int clientId, CancellationToken token)
+    {
+        return await _context.Clients.AnyAsync(c => c.Id == clientId, token);
+    }
+}
