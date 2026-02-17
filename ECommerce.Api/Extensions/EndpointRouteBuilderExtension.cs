8a5685ceb7bd57c9a61b579f3677aa1bb81b8e08
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-02-17 18:41:44 -0400

    Add EndpointRouteBuilderExtension.cs

---
+++ ECommerce.Api/Extensions/EndpointRouteBuilderExtension.cs
@@ -0,0 +1,11 @@
+﻿using ECommerce.Api.Application.Endpoints;
+
+namespace ECommerce.Api.Extensions;
+
+public static class EndpointRouteBuilderExtension
+{
+    public static void MapApiEndpoints(this IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapAuthEndpoints();
+    }
+}
