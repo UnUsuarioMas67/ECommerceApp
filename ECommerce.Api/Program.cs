2184149e0bfa106c4bb5371f3a47d31148438e57
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-02-17 18:50:47 -0400

    Update Program.cs to call new extension methods

--- ECommerce.Api/Program.cs
+++ ECommerce.Api/Program.cs
@@ -1,9 +1,6 @@
-using System.Text;
-using ECommerce.Api.Application.Auth;
+using ECommerce.Api.Extensions;
 using ECommerce.Api.Infrastructure.EF;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,48 +12,16 @@ builder.Services.AddOpenApi();
 builder.Services.AddDbContext<ECommerceContext>(o =>
     o.UseSqlServer(builder.Configuration.GetConnectionString("ECommerceDb")));
 
-builder.Services.Configure<JwtSettings>(
-    builder.Configuration.GetSection("JwtSettings"));
-builder.Services.AddAuthentication(o =>
-    {
-        o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-        o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    })
-    .AddJwtBearer(o =>
-    {
-        var jwtSettings = builder.Configuration.GetSection("JwtSettings").Get<JwtSettings>();
-
-        o.TokenValidationParameters = new TokenValidationParameters
-        {
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings!.SecretKey)),
-            ValidIssuer = jwtSettings.Issuer,
-            ValidAudience = jwtSettings.Audience,
-            ValidateAudience = true,
-            ValidateIssuer = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-        };
-    });
-builder.Services.AddScoped<IJwtService, JwtService>();
-builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
-
-builder.Services.AddAuthorization(o =>
-{
-    o.AddPolicy(UserRoles.Client, policy => policy.RequireRole(UserRoles.Client, UserRoles.Admin));
-    o.AddPolicy(UserRoles.Admin, policy => policy.RequireRole(UserRoles.Admin));
-});
-
-// builder.Services.AddSwaggerGenWithAuth();
+builder.Services.ConfigureAuth(builder.Configuration.GetSection("JwtSettings"));
 
 var app = builder.Build();
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
     app.MapScalarApiReference();
-    
+
     using var scope = app.Services.CreateScope();
     var dbContext = scope.ServiceProvider.GetRequiredService<ECommerceContext>();
     dbContext.Database.Migrate();
@@ -67,5 +32,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapGet("/test", (ECommerceContext dbContext) => dbContext.OrderStatuses.ToList());
+
+app.MapApiEndpoints();
 
 app.Run();
