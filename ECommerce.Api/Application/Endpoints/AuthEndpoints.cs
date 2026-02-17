6e6408e89ed4bf346833e77e70398914367cef41
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-02-17 18:40:55 -0400

    Add AuthEndpoints.cs

---
+++ ECommerce.Api/Application/Endpoints/AuthEndpoints.cs
@@ -0,0 +1,34 @@
+﻿using ECommerce.Api.Application.Auth;
+using ECommerce.Api.Application.DTOs;
+using ECommerce.Api.Application.DTOs.Auth;
+using ECommerce.Api.Domain.Entities;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace ECommerce.Api.Application.Endpoints;
+
+public static class AuthEndpoints
+{
+    public static IEndpointRouteBuilder MapAuthEndpoints(this IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapPost("api/login/client", LoginClient);
+        endpoints.MapPost("api/login/admin", LoginAdmin);
+        
+        return endpoints;
+    }
+    
+    private static async Task<Ok<AuthenticationDto>> LoginClient(
+        IAuthenticationService authenticationService,
+        LoginRequestDto requestDto)
+    {
+        var result = await authenticationService.Login<Client>(requestDto.Email, requestDto.Password);
+        return TypedResults.Ok(result.ToDto());
+    }
+    
+    private static async Task<Ok<AuthenticationDto>> LoginAdmin(
+        IAuthenticationService authenticationService, 
+        LoginRequestDto requestDto)
+    {
+        var result = await authenticationService.Login<Admin>(requestDto.Email, requestDto.Password);
+        return TypedResults.Ok(result.ToDto());
+    }
+}
