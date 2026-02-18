dffe7bf18a97275df6287f754c54a0b9791933d3
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-02-18 16:30:04 -0400

    Update DtoConversion.cs

--- ECommerce.Api/Application/DTOs/DtoConversion.cs
+++ ECommerce.Api/Application/DTOs/DtoConversion.cs
@@ -27,4 +27,33 @@ public static class DtoConversion
             Token = result.Value?.Token,
             User = result.Value?.User.ToDto()
         };
+
+    public static Client ToClientEntity(this CreateUserDto dto)
+        => new Client
+        {
+            FirstName = dto.FirstName,
+            LastName = dto.LastName,
+            Email = dto.Email,
+            PasswordHash = PasswordHasher.HashPassword(dto.Password),
+            PhoneNumber = dto.PhoneNumber,
+            BirthDate = DateOnly.Parse(dto.BirthDate),
+            CreatedAt = DateTime.UtcNow,
+        };
+
+    public static void UpdateFromDto(this Client client, UpdateUserDto dto)
+    {
+        client.FirstName = !string.IsNullOrEmpty(dto.FirstName) ? dto.FirstName : client.FirstName;
+
+        client.LastName = !string.IsNullOrEmpty(dto.LastName) ? dto.LastName : client.LastName;
+
+        client.PhoneNumber = !string.IsNullOrEmpty(dto.PhoneNumber) ? dto.PhoneNumber : client.PhoneNumber;
+
+        client.BirthDate = !string.IsNullOrWhiteSpace(dto.BirthDate)
+            ? DateOnly.Parse(dto.BirthDate)
+            : client.BirthDate;
+
+        client.PasswordHash = !string.IsNullOrWhiteSpace(dto.Password)
+            ? PasswordHasher.HashPassword(dto.Password)
+            : client.PasswordHash;
+    }
 }
