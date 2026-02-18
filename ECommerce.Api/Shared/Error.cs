5f9128a2ac3e43966c59dbbcaab2d0874144ac5b
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-02-17 21:45:10 -0400

    Add Error.cs
    This class will be used in the Result class rewrite.

---
+++ ECommerce.Api/Shared/Error.cs
@@ -0,0 +1,18 @@
+﻿namespace ECommerce.Api.Shared;
+
+public record Error(ErrorType ErrorType, IDictionary<string, string[]> Details);
+
+public static class Errors
+{
+    public static Error NotFound()
+        => new Error(ErrorType.NotFound, new Dictionary<string, string[]>());
+
+    public static Error ValidationError(IDictionary<string, string[]> details)
+        => new Error(ErrorType.Validation, details); 
+}
+
+public enum ErrorType
+{
+    Validation,
+    NotFound
+}
