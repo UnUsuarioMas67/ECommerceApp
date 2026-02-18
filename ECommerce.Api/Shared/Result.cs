292a937623388692e8890293cd2cdb0e9531b492
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-02-17 20:36:58 -0400

    Update Result.cs
    This changes the type of the Errors property from Dictionary to IDictionary, so it works with the ToDictionary() method of the ValidationResult class from FluentValidation.

--- ECommerce.Api/Shared/Result.cs
+++ ECommerce.Api/Shared/Result.cs
@@ -4,10 +4,10 @@ public class Result<T>
 {
     public bool IsSuccess { get; }
     public T? Value { get; }
-    public Dictionary<string, string[]> Errors { get; }
+    public IDictionary<string, string[]> Errors { get; }
     public string? ErrorMessage => Errors.FirstOrDefault().Value.FirstOrDefault();
 
-    public Result(bool success, T? value, Dictionary<string, string[]>? errors)
+    public Result(bool success, T? value, IDictionary<string, string[]>? errors)
     {
         IsSuccess = success;
         Value = value;
@@ -19,6 +19,9 @@ public static class Result
 {
     public static Result<T> Success<T>(T value)
         => new Result<T>(true, value, null);
+    
+    public static Result<T> Failure<T>(IDictionary<string, string[]> error)
+        => new Result<T>(false, default, error);
 
     public static Result<T> Failure<T>(string error)
     {
@@ -29,7 +32,4 @@ public static class Result
 
         return new Result<T>(false, default, errors);
     }
-
-    public static Result<T> Failure<T>(Dictionary<string, string[]> error)
-        => new Result<T>(false, default, error);
 }
