0911b08c0deefa8f7085e1a682d53c46b1cdde4e
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-02-18 16:27:34 -0400

    Add CreateUserDto.cs and UpdateUserDto.cs

---
+++ ECommerce.Api/Application/DTOs/User/UpdateUserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerce.Api.Application.DTOs.User;
+
+public class UpdateUserDto
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Password { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? BirthDate { get; set; }
+}
