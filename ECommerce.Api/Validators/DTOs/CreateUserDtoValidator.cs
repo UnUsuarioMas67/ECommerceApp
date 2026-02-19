8bfcb1949879b36802a876a23d59dc7bd8cb089f
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-02-18 21:12:15 -0400

    Add CreateUserDtoValidator.cs and UpdateUserDtoValidator.cs

---
+++ ECommerce.Api/Validators/DTOs/CreateUserDtoValidator.cs
@@ -0,0 +1,37 @@
+﻿using ECommerce.Api.Application.DTOs.User;
+using ECommerce.Api.Domain.Validation;
+using ECommerce.Api.Extensions;
+using FluentValidation;
+
+namespace ECommerce.Api.Validators.DTOs;
+
+public class CreateUserDtoValidator : AbstractValidator<CreateUserDto>
+{
+    public CreateUserDtoValidator()
+    {
+        RuleFor(u => u.Password)
+            .NotEmpty()
+            .MinimumLength(TextLengthRules.PasswordMinLenght);
+        
+        RuleFor(u => u.FirstName)
+            .NotEmpty()
+            .MaximumLength(TextLengthRules.Name);
+
+        RuleFor(u => u.LastName)
+            .NotEmpty()
+            .MaximumLength(TextLengthRules.Name);
+
+        RuleFor(u => u.Email)
+            .NotEmpty()
+            .EmailAddress()
+            .MaximumLength(TextLengthRules.Email);
+
+        RuleFor(u => u.PhoneNumber)
+            .NotEmpty()
+            .MaximumLength(TextLengthRules.PhoneNumber);
+
+        RuleFor(u => u.BirthDate)
+            .NotNull()
+            .CanParseIntoDate();
+    }
+}
---
+++ ECommerce.Api/Validators/DTOs/UpdateUserDtoValidator.cs
@@ -0,0 +1,35 @@
+﻿using ECommerce.Api.Application.DTOs.User;
+using ECommerce.Api.Domain.Validation;
+using ECommerce.Api.Extensions;
+using FluentValidation;
+
+namespace ECommerce.Api.Validators.DTOs;
+
+public class UpdateUserDtoValidator : AbstractValidator<UpdateUserDto>
+{
+    public UpdateUserDtoValidator()
+    {
+        RuleFor(u => u.Password)
+            .MinimumLength(TextLengthRules.PasswordMinLenght)
+            .Unless(u => u.Password == null);
+        
+        RuleFor(u => u.FirstName)
+            .NotEmpty()
+            .Unless(u => u.FirstName == null)
+            .MaximumLength(TextLengthRules.Name);
+
+        RuleFor(u => u.LastName)
+            .NotEmpty()
+            .Unless(u => u.LastName == null)
+            .MaximumLength(TextLengthRules.Name);
+
+        RuleFor(u => u.PhoneNumber)
+            .NotEmpty()
+            .Unless(u => u.PhoneNumber == null)
+            .MaximumLength(TextLengthRules.PhoneNumber);
+
+        RuleFor(u => u.BirthDate)
+            .CanParseIntoDate()
+            .Unless(u => u.BirthDate == null);
+    }
+}
