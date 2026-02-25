080d8d8d5b23d8f0d17d3d6aa2fb162b3f85377b
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-02-25 03:21:10 -0400

    Remove unneeded rules from validators

--- ECommerce.Api/Validators/DTOs/CreateAddressDtoValidator.cs
+++ ECommerce.Api/Validators/DTOs/CreateAddressDtoValidator.cs
@@ -10,28 +10,9 @@ public class CreateAddressDtoValidator : AbstractValidator<CreateAddressDto>
     {
         RuleFor(a => a.ClientId)
             .NotEmpty();
-        
-        RuleFor(a => a.AddressLine1)
-            .NotEmpty()
-            .MaximumLength(TextLengthRules.ShortText);
-        
-        RuleFor(a => a.AddressLine2)
-            .MaximumLength(TextLengthRules.ShortText);
 
         RuleFor(a => a.CountryCode)
             .NotEmpty()
             .MaximumLength(2);
-
-        RuleFor(a => a.Region)
-            .NotEmpty()
-            .MaximumLength(TextLengthRules.ShortText);
-        
-        RuleFor(a => a.City)
-            .NotEmpty()
-            .MaximumLength(TextLengthRules.ShortText);
-
-        RuleFor(a => a.PostalCode)
-            .NotEmpty()
-            .MaximumLength(TextLengthRules.PostalCode);
     }
 }
--- ECommerce.Api/Validators/DTOs/UpdateAddressDtoValidator.cs
+++ ECommerce.Api/Validators/DTOs/UpdateAddressDtoValidator.cs
@@ -8,32 +8,9 @@ public class UpdateAddressDtoValidator : AbstractValidator<UpdateAddressDto>
 {
     public UpdateAddressDtoValidator()
     {
-        RuleFor(a => a.AddressLine1)
-            .NotEmpty()
-            .Unless(a => a.AddressLine1 == null)
-            .MaximumLength(TextLengthRules.ShortText);
-
-        RuleFor(a => a.AddressLine2)
-            .MaximumLength(TextLengthRules.ShortText);
-
         RuleFor(a => a.CountryCode)
             .NotEmpty()
             .Unless(a => a.CountryCode == null)
             .MaximumLength(2);
-
-        RuleFor(a => a.Region)
-            .NotEmpty()
-            .Unless(a => a.Region == null)
-            .MaximumLength(TextLengthRules.ShortText);
-
-        RuleFor(a => a.City)
-            .NotEmpty()
-            .Unless(a => a.City == null)
-            .MaximumLength(TextLengthRules.ShortText);
-
-        RuleFor(a => a.PostalCode)
-            .NotEmpty()
-            .Unless(a => a.PostalCode == null)
-            .MaximumLength(TextLengthRules.PostalCode);
     }
 }
