4c2eaf5b7a9d87cad09aaa65078cab76f5971401
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-02-26 22:44:05 -0400

    Update AddressResponseDto.cs

--- ECommerce.Api/Application/DTOs/Address/AddressResponseDto.cs
+++ ECommerce.Api/Application/DTOs/Address/AddressResponseDto.cs
@@ -6,10 +6,10 @@ public class AddressResponseDto
     public int? ClientId { get; set; }
     
     public string AddressLine1 { get; set; }
-    public string? AddressLine2 { get; set; }
+    public string AddressLine2 { get; set; }
 
     public string Country { get; set; }
-    public string? Region { get; set; }
+    public string Region { get; set; }
     public string City { get; set; }
     public string PostalCode { get; set; }
 }
