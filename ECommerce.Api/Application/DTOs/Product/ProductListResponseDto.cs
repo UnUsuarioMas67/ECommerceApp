7f59947a9227ffac72f16158b0920778577e9ab0
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-03-03 01:19:18 -0400

    Add DTO to display product list alongside additional information
    Other endpoints that return lists as response will receive a similar treatment in the future (hopefully tomorrow).

---
+++ ECommerce.Api/Application/DTOs/Product/ProductListResponseDto.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+using ECommerce.Api.Application.DTOs.Shared;
+
+namespace ECommerce.Api.Application.DTOs.Product;
+
+public class ProductListResponseDto
+{
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public string? Category { get; set; }
+    
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public string? SearchTerm { get; set; }
+
+    public int Total => Products.Count();
+    public required PaginationInfo Pagination { get; set; }
+    public required IEnumerable<ProductResponseDto> Products { get; set; }
+}
--- ECommerce.Api/Application/DTOs/Shared/PaginationQuery.cs
+++ ECommerce.Api/Application/DTOs/Shared/PaginationQuery.cs
@@ -8,6 +8,12 @@ public class PaginationQuery
 
 public static class PaginationDefaults
 {
-    public static int Skip = 0;
-    public static int Limit = 100;
+    public const int Skip = 0;
+    public const int Limit = 100;
+}
+
+public record PaginationInfo(int Skip, int? Limit)
+{
+    public static implicit operator PaginationInfo(PaginationQuery query)
+        => new(query.Skip ?? PaginationDefaults.Skip, query.Limit);
 }
