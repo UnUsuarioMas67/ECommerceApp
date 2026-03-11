ecb89e88971109a864158a99764b38a51fc6757d
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-03-11 02:03:21 -0400

    Remove commented code

--- ECommerce.Api/Application/Services/Auth/AuthenticationService.cs
+++ ECommerce.Api/Application/Services/Auth/AuthenticationService.cs
@@ -12,24 +12,9 @@ public interface IAuthenticationService
     Task<Result<AuthenticationDto>> LoginAdmin(string email, string password);
 }
 
-public class AuthenticationService(IJwtService jwtService, ECommerceContext context, IClientMapper mapper)
+public class AuthenticationService(IJwtService jwtService, ECommerceContext context, ClientMapper mapper)
     : IAuthenticationService
 {
-    // public async Task<Result<AuthenticationDto>> Login<TUser>(string email, string password) where TUser : class, IUser
-    // {
-    //     var dbSet = dbContext.Set<TUser>();
-    //     var user = await dbSet.FirstOrDefaultAsync(u => u.Email == email);
-    //     if (user == null)
-    //         return Errors.AuthenticationError("Login", "Invalid credentials");
-    //
-    //     var passwordValid = BCrypt.Net.BCrypt.Verify(password, user.PasswordHash);
-    //     if (!passwordValid)
-    //         return Errors.AuthenticationError("Login", "Invalid credentials");
-    //
-    //     var token = jwtService.GenerateAccessToken(user);
-    //     return new AuthenticationDto { Token = token, User = user };
-    // }
-
     public async Task<Result<AuthenticationDto>> LoginClient(string email, string password)
     {
         var client = await context.Clients.FirstOrDefaultAsync(c => c.Email == email);
@@ -41,7 +26,7 @@ public class AuthenticationService(IJwtService jwtService, ECommerceContext cont
             return Errors.AuthenticationError("Login", "Invalid credentials");
 
         var token = jwtService.GenerateAccessToken(client);
-        return new AuthenticationDto { Token = token, User = mapper.ToDto(client) };
+        return new AuthenticationDto { Token = token, User = mapper.MapToDto(client) };
     }
 
     public Task<Result<AuthenticationDto>> LoginAdmin(string email, string password)
