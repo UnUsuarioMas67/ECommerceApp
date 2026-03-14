3f85090ad3babfd6d36525f101effb05f4228832
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-03-13 21:12:29 -0400

    Update CartItem entity
    It no longer has an Id property. Instead, it will use CartId and ProductId as a composite primary key

---
+++ ECommerce.Api/Infrastructure/EF/Configuration/CartItemConfig.cs
@@ -0,0 +1,14 @@
+﻿using ECommerce.Api.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ECommerce.Api.Infrastructure.EF.Configuration;
+
+public class CartItemConfig : IEntityTypeConfiguration<CartItem>
+{
+    public void Configure(EntityTypeBuilder<CartItem> builder)
+    {
+        builder
+            .HasKey(x => new { x.CartId, x.ProductId });
+    }
+}
