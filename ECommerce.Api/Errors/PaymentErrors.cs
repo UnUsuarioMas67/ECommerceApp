5369dec0d48c3e73830d4e8d8a13a37d967e36e4
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-03-26 00:11:04 -0400

    Add PaymentErrors.cs

---
+++ ECommerce.Api/Errors/PaymentErrors.cs
@@ -0,0 +1,5 @@
+﻿namespace ECommerce.Api.Errors;
+
+public record OrderNotExistsError(int OrderId) : Error($"ShopOrder with id {OrderId} not found");
+public record OrderAlreadyPaid(int OrderId) 
+    : Error($"ShopOrder with id {OrderId} already has a payment attached");
