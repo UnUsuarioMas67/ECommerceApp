8176767dae3777767bf88753effcdf4739bf8251
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-03-27 02:21:59 -0400

    Add CheckoutRequestDtoValidator.cs

---
+++ ECommerce.Api/Validators/DTOs/Checkout/CheckoutRequestDtoValidator.cs
@@ -0,0 +1,22 @@
+﻿using ECommerce.Api.Application.DTOs.Checkout;
+using FluentValidation;
+
+namespace ECommerce.Api.Validators.DTOs.Checkout;
+
+public class CheckoutRequestDtoValidator : AbstractValidator<CheckoutRequestDto>
+{
+    public CheckoutRequestDtoValidator()
+    {
+        RuleFor(r => r.CartId)
+            .GreaterThan(0)
+            .NotNull();
+
+        RuleFor(r => r.AddressId)
+            .GreaterThan(0)
+            .NotNull();
+        
+        RuleFor(r => r.SuccessUrl)
+            .NotEmpty()
+            .WithMessage("SuccessUrl is required");
+    }
+}
