242813f2a0a15e3ba652c5a5ab301b8fa19e4445
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-03-30 02:20:15 -0400

    Update Result.cs and Error.cs
    This changes Value and Error properties of Result class to no longer be nullable and eliminate annoying null value warnings.

--- ECommerce.Api/Errors/Error.cs
+++ ECommerce.Api/Errors/Error.cs
@@ -1,6 +1,9 @@
 ﻿namespace ECommerce.Api.Errors;
 
-public record Error(string Message);
+public record Error(string Message)
+{
+    public static Error None() => new Error("None");
+}
 public record ValidationError(IDictionary<string, string[]> Details) : Error("One or more validation errors occurred");
 public record NotFoundError() : Error("Could not find the requested resource");
 public record ClientNotExistsError(int ClientId, object? Identifier) : Error("The specified client doesn't seem to exist");
--- ECommerce.Api/Shared/Result.cs
+++ ECommerce.Api/Shared/Result.cs
@@ -5,12 +5,12 @@ namespace ECommerce.Api.Shared;
 public record Result
 {
     public bool IsSuccess { get; }
-    public Error? Error { get; }
+    public Error Error { get; }
 
     protected Result(bool isSuccess, Error? error)
     {
         IsSuccess = isSuccess;
-        Error = error;
+        Error = error ?? Error.None();
     }
 
     public static Result Success() => new(true, null);
@@ -21,18 +21,17 @@ public record Result
 
 public record Result<T> : Result
 {
-    public T? Value { get; }
+    private readonly T? _value;
+    public T Value => IsSuccess
+            ? _value ?? throw new InvalidOperationException($"{nameof(Value)} cannot be null")
+            : throw new InvalidOperationException("Tried to get a Value from a failed Result");
 
-    private Result(T value) : base(true, null) => Value = value;
+    private Result(T value) : base(true, null) => _value = value;
+    private Result(Error error) : base(false, error) { }
 
-    private Result(Error error) : base(false, error)
-    {
-    }
-
-    public static Result<T> Success(T value) => new(value);
+    public static Result<T> Success(T value) => new(value ?? throw new ArgumentNullException(nameof(value)));
     public new static Result<T> Failure(Error error) => new(error ?? throw new ArgumentNullException(nameof(error)));
 
     public static implicit operator Result<T>(T value) => new(value);
-
     public static implicit operator Result<T>(Error error) => new(error);
 }
