16ad05780fb1c2b64215a750db5c6d3a204df64b
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-04-12 01:22:25 -0400

    Update Payment.cs

--- ECommerce.Api/Entities/Payment.cs
+++ ECommerce.Api/Entities/Payment.cs
@@ -1,26 +1,15 @@
 namespace ECommerce.Api.Entities;
 
-public enum PaymentStatus
-{
-    Pending = 1,
-    Succeeded = 2,
-    Failed = 3
-}
-
 public class Payment
 {
     public int Id { get; set; }
     public string StripeSessionId { get; set; } = string.Empty;
-    
+
     public decimal Amount { get; set; }
     public string Currency { get; set; } = "usd";
-    
-    // public int StatusId { get; set; } = (int)PaymentStatus.Pending;
-    // public PaymentStatus Status { get; set; } = PaymentStatus.Pending;
-    
+
     public int OrderId { get; set; }
     public ShopOrder Order { get; set; }
-    
+
     public DateTime CreatedAt { get; set; }
-    // public DateTime? UpdatedAt { get; set; }
 }
