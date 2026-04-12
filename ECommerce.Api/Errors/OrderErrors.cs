9829d5b29c022393662a0166bc7bdb47d8da01dc
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-04-12 00:27:04 -0400

    Add WebhookError

--- ECommerce.Api/Errors/OrderErrors.cs
+++ ECommerce.Api/Errors/OrderErrors.cs
@@ -12,3 +12,5 @@ public record AddressNotFoundError(int AddressId)
 public record ProductsStockError(ICollection<ProductsStockErrorItem> Products)
     : Error("One or more products have insufficient stock");
 public record ProductsStockErrorItem(int ProductId, int QuantityRequested, int StockAvailable);
+
+public record WebhookError() : Error("Webhook processing failed");
