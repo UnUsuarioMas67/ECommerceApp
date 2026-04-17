2cbb5cffa5bfb9480591279aa91a3f0ba4f93787
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-04-17 18:18:14 -0400

    Add RefreshRequestDto and it's validator

---
+++ ECommerce.Api/DTOs/Auth/RefreshRequestDto.cs
@@ -0,0 +1,6 @@
+﻿namespace ECommerce.Api.DTOs.Auth;
+
+public class RefreshRequestDto
+{
+    public string RefreshToken { get; set; }
+}
---
+++ ECommerce.Api/Validators/DTOs/Auth/RefreshRequestDtoValidator.cs
@@ -0,0 +1,14 @@
+﻿using ECommerce.Api.DTOs.Auth;
+using FluentValidation;
+
+namespace ECommerce.Api.Validators.DTOs.Auth;
+
+public class RefreshRequestDtoValidator : AbstractValidator<RefreshRequestDto>
+{
+    public RefreshRequestDtoValidator()
+    {
+        RuleFor(p => p.RefreshToken)
+            .NotEmpty()
+            .WithMessage("Refresh token is required");
+    }
+}
