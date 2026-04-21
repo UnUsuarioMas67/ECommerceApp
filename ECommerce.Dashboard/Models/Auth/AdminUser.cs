fa53cf5729f3f324aa21793f522ed628d2b9440c
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-04-20 23:45:11 -0400

    Add AdminUser.cs

---
+++ ECommerce.Dashboard/Models/Auth/AdminUser.cs
@@ -0,0 +1,14 @@
+﻿namespace ECommerce.Dashboard.Models.Auth;
+
+public class AdminUser
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public DateOnly BirthDate { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+    public string FullName => FirstName + " " + LastName;
+}
