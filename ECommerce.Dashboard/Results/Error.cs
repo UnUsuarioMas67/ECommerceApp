171b9062202a6aa907426346e9e0deabe37d1022
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-04-21 21:34:50 -0400

    Add more errors

--- ECommerce.Dashboard/Results/Error.cs
+++ ECommerce.Dashboard/Results/Error.cs
@@ -1,6 +1,13 @@
 ﻿namespace ECommerce.Dashboard.Results;
 
-public record Error(string Message)
+public record Error(string Type, string Message)
 {
-    public static Error None() => new Error("None");
+    public static Error None() => new Error("none", "No error has occurred");
 }
+
+public record LoginCredentialsError() : Error("login_credentials", "Invalid email or password");
+
+public record UnexpectedApiResponseError(int StatusCode, Dictionary<string, object>? ResponseBody = null)
+    : Error("api_unexpected_response", "Unexpected API response");
+
+public record ApiServerError(int StatusCode = 500) : Error("api_server_error", "Request failed due to API server error");
