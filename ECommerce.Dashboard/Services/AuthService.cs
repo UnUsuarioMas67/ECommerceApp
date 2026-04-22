ea2f86f30b96e8d4547f25d7870a63314a711309
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-04-21 21:35:47 -0400

    Add AuthService.cs

---
+++ ECommerce.Dashboard/Services/AuthService.cs
@@ -0,0 +1,60 @@
+﻿using System.Net;
+using System.Text.Json;
+using ECommerce.Dashboard.Models.Auth;
+using ECommerce.Dashboard.Results;
+
+namespace ECommerce.Dashboard.Services;
+
+public class AuthService(IHttpClientFactory clientFactory, ILogger<AuthService> logger)
+{
+    private const string LoginPath = "api/admins/login";
+    private const string LogoutPath = "api/admins/logout";
+    private const string RefreshPath = "api/admins/refresh";
+
+    private readonly HttpClient _httpClient = clientFactory.CreateClient("ApiClient");
+
+    public async Task<Result<UserLoginResponse>> LoginAsync(UserLoginRequest request)
+    {
+        logger.LogInformation("Requesting login: {email}", request.Email);
+
+        var response = await _httpClient.PostAsJsonAsync(LoginPath, request);
+        if (response.IsSuccessStatusCode)
+        {
+            var userLogin = await response.Content.ReadFromJsonAsync<UserLoginResponse>()
+                            ?? throw new InvalidOperationException("Could not deserialize the response");
+            logger.LogInformation("Login request successful. User: {admin}", userLogin.User.Email);
+            return userLogin;
+        }
+
+        if (response.StatusCode == HttpStatusCode.Unauthorized)
+        {
+            logger.LogInformation("Login request failed. Invalid login credentials");
+            return new LoginCredentialsError();
+        }
+
+        if ((int)response.StatusCode >= 500)
+        {
+            var error = new ApiServerError((int)response.StatusCode);
+            logger.LogWarning("{error}", error.Message);
+            return error;
+        }
+
+        var errorBody = await response.Content.ReadFromJsonAsync<Dictionary<string, object>>();
+        var responseError = new UnexpectedApiResponseError((int)response.StatusCode, errorBody);
+        
+        var json = JsonSerializer.Serialize(responseError, new JsonSerializerOptions { WriteIndented = true });
+        logger.LogWarning("Unexpected response: {error}", json);
+        
+        return responseError;
+    }
+    
+    public async Task<Result<UserLoginResponse>> RefreshAsync(string token)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public async Task<Result> LogoutAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
