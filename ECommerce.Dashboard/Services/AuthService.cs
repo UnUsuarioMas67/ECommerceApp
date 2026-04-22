59e302332465c6771ff4397e0296726768353084
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-04-22 19:27:53 -0400

    Add RefreshAsync method

--- ECommerce.Dashboard/Services/AuthService.cs
+++ ECommerce.Dashboard/Services/AuthService.cs
@@ -50,11 +50,65 @@ public class AuthService(IHttpClientFactory clientFactory, ILogger<AuthService> 
     
     public async Task<Result<UserLoginResponse>> RefreshAsync(string token)
     {
-        throw new NotImplementedException();
+        var response = await _httpClient.PostAsJsonAsync(RefreshPath, new {RefreshToken = token});
+        if (response.IsSuccessStatusCode)
+        {
+            var userLogin = await response.Content.ReadFromJsonAsync<UserLoginResponse>()
+                            ?? throw new InvalidOperationException("Could not deserialize the response");
+            logger.LogDebug("Refresh request successful. User: {admin}", userLogin.User.Email);
+            return userLogin;
+        }
+        
+        if (response.StatusCode == HttpStatusCode.Unauthorized)
+        {
+            logger.LogInformation("Refresh request failed. Invalid or expired refresh token");
+            return new RefreshTokenError();
+        }
+        
+        if ((int)response.StatusCode >= 500)
+        {
+            var error = new ApiServerError((int)response.StatusCode);
+            logger.LogWarning("{error}", error.Message);
+            return error;
+        }
+
+        var errorBody = await response.Content.ReadFromJsonAsync<Dictionary<string, object>>();
+        var responseError = new UnexpectedApiResponseError((int)response.StatusCode, errorBody);
+        
+        var json = JsonSerializer.Serialize(responseError, new JsonSerializerOptions { WriteIndented = true });
+        logger.LogWarning("Unexpected response: {error}", json);
+        
+        return responseError;
     }
     
     public async Task<Result> LogoutAsync()
     {
         throw new NotImplementedException();
+
+        // var response = await _httpClient.PostAsync(LogoutPath, null);
+        // if (response.IsSuccessStatusCode)
+        // {
+        //     return Result.Success();
+        // }
+        //
+        // if (response.StatusCode == HttpStatusCode.Unauthorized)
+        // {
+        //     return new RefreshTokenError();
+        // }
+        //
+        // if ((int)response.StatusCode >= 500)
+        // {
+        //     var error = new ApiServerError((int)response.StatusCode);
+        //     logger.LogWarning("{error}", error.Message);
+        //     return error;
+        // }
+        //
+        // var errorBody = await response.Content.ReadFromJsonAsync<Dictionary<string, object>>();
+        // var responseError = new UnexpectedApiResponseError((int)response.StatusCode, errorBody);
+        //
+        // var json = JsonSerializer.Serialize(responseError, new JsonSerializerOptions { WriteIndented = true });
+        // logger.LogWarning("Unexpected response: {error}", json);
+        //
+        // return responseError;
     }
 }
