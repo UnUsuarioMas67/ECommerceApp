1c918bed29649677d76c043fb491c4b5ed10858e
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-04-22 21:44:08 -0400

    Add custom exceptions

---
+++ ECommerce.Dashboard/Exceptions/ApiServerException.cs
@@ -0,0 +1,6 @@
+﻿using System.Net;
+
+namespace ECommerce.Dashboard.Exceptions;
+
+public class ApiServerException(HttpStatusCode statusCode)
+    : HttpRequestException("Request failed due to API server error", null, statusCode);
---
+++ ECommerce.Dashboard/Exceptions/UnexpectedApiResponseException.cs
@@ -0,0 +1,9 @@
+﻿using System.Net;
+
+namespace ECommerce.Dashboard.Exceptions;
+
+public class UnexpectedApiResponseException(HttpStatusCode statusCode, IDictionary<string, object>? responseBody = null)
+    : HttpRequestException("Unexpected response from API", null, statusCode)
+{
+    public IDictionary<string, object>? ResponseBody { get; } = responseBody;
+}
