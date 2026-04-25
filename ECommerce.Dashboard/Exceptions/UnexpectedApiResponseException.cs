938b8a49ef2e4fbb347a264e57bd3cfc8652b7e6
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-04-25 00:50:14 -0400

    Update UnexpectedApiResponseException.cs

--- ECommerce.Dashboard/Exceptions/UnexpectedApiResponseException.cs
+++ ECommerce.Dashboard/Exceptions/UnexpectedApiResponseException.cs
@@ -2,8 +2,8 @@
 
 namespace ECommerce.Dashboard.Exceptions;
 
-public class UnexpectedApiResponseException(HttpStatusCode statusCode, IDictionary<string, object>? responseBody = null)
+public class UnexpectedApiResponseException(HttpStatusCode statusCode)
     : HttpRequestException("Unexpected response from API", null, statusCode)
 {
-    public IDictionary<string, object>? ResponseBody { get; } = responseBody;
+    
 }
