e218899c7e331cec53887f7d00bbfeb2c839b2c4
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-04-29 19:48:59 -0400

    Add and implement LoginViewModel.cs

--- ECommerce.Dashboard/Controllers/AccountController.cs
+++ ECommerce.Dashboard/Controllers/AccountController.cs
@@ -1,4 +1,4 @@
-﻿using ECommerce.Dashboard.Models.Auth;
+﻿using ECommerce.Dashboard.Models;
 using ECommerce.Dashboard.Services;
 using ECommerce.Dashboard.Services.Api;
 using Microsoft.AspNetCore.Mvc;
@@ -10,19 +10,19 @@ public class AccountController(ApiAuthService apiAuthService, SiteAuthService si
     [HttpGet]
     public IActionResult Login()
     {
-        var request = new UserLoginRequest();
-        return View(request);
+        var model = new LoginViewModel();
+        return View(model);
     }
 
     [HttpPost]
-    public async Task<IActionResult> Login(UserLoginRequest request)
+    public async Task<IActionResult> Login(LoginViewModel model)
     {
         if (!ModelState.IsValid)
-            return View(request);
+            return View(model);
 
-        var loginResult = await apiAuthService.LoginAsync(request);
+        var loginResult = await apiAuthService.LoginAsync(model.LoginRequest);
         if (!loginResult.IsSuccess)
-            return View();
+            return View(model);
 
         var login = loginResult.Value;
         await siteAuthService.SignInAsync(login.User, login.AccessToken, login.RefreshToken);
---
+++ ECommerce.Dashboard/Models/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿using ECommerce.Dashboard.Models.Auth;
+
+namespace ECommerce.Dashboard.Models;
+
+public class LoginViewModel
+{
+    public UserLoginRequest LoginRequest { get; set; } = new();
+}
