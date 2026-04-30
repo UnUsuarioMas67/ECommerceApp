3abf63f3267c1f4bbe49a456496976959bc0c2d2
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-04-30 19:09:07 -0400

    Add Product DTOs

---
+++ ECommerce.Dashboard/DTOs/Product/ProductResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace ECommerce.Dashboard.DTOs.Product;
+
+public class ProductResponse
+{
+    public int Id { get; set; }
+    
+    public string? Category { get; set; }
+    
+    public string Sku { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public int Stock { get; set; }
+    
+    public string? ImageUrl { get; set; }
+}
---
+++ ECommerce.Dashboard/DTOs/Product/ProductUpdate.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce.Dashboard.DTOs.Product;
+
+public class ProductUpdate
+{
+    [Required]
+    [StringLength(50)]
+    public string Category { get; set; } = string.Empty;
+    
+    [Required]
+    [StringLength(100)]
+    public string Name { get; set; } = string.Empty;
+    
+    [Required]
+    [StringLength(1000)]
+    [DataType(DataType.MultilineText)]
+    public string Description { get; set; } = string.Empty;
+    
+    [Required]
+    [DataType(DataType.Currency)]
+    [Range(0, 10_000_000)]
+    public decimal Price { get; set; }
+    
+    [DataType(DataType.Url)]
+    public string? ImageUrl { get; set; }
+}
