6087d649543f5fe652e9a79a3b3ef2c020a4b0b6
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-04-30 19:09:24 -0400

    Add Category DTOs

---
+++ ECommerce.Dashboard/DTOs/Category/CategoryResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace ECommerce.Dashboard.DTOs.Category;
+
+public class CategoryResponse
+{
+    public int Id { get; set; }
+    public string Slug { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+}
