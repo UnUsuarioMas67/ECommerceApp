377ca0f3ffc2d633962dbcb0a35dfa62a62ab5e1
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-05-01 20:29:46 -0400

    Add ApiError.cs

---
+++ ECommerce.Dashboard/DTOs/Error/ApiError.cs
@@ -0,0 +1,8 @@
+﻿namespace ECommerce.Dashboard.DTOs.Error;
+
+public class ApiError
+{
+    public string ErrorType { get; set; } = string.Empty;
+    public string Message { get; set; } = string.Empty;
+    public IDictionary<string, object?> Details { get; set; } = new Dictionary<string, object?>();
+}
