a74c88a1990ce95d28c1c89d7dc778a4aebd8141
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-05-02 19:52:50 -0400

    Add ProductListViewModel.cs

---
+++ ECommerce.Dashboard/Models/ProductListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ECommerce.Dashboard.DTOs.Category;
+using ECommerce.Dashboard.DTOs.Product;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ECommerce.Dashboard.Models;
+
+public class ProductListViewModel
+{
+    public List<ProductResponse> Products { get; set; } = new List<ProductResponse>();
+    public SelectList CategorySelect { get; set; } = new SelectList(new List<CategoryResponse>(), "Slug", "Name");
+    [FromQuery(Name = "category")] public string? Category { get; set; }
+    [FromQuery(Name = "searchTerm")] public string? SearchTerm { get; set; }
+}
