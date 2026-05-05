5830b0ec189f449ea3055821327346380fb30092
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-05-04 23:26:34 -0400

    Add CategoriesController.cs and create it's Index view

---
+++ ECommerce.Dashboard/Controllers/CategoriesController.cs
@@ -0,0 +1,27 @@
+﻿using ECommerce.Dashboard.DTOs.Shared;
+using ECommerce.Dashboard.Models.Categories;
+using ECommerce.Dashboard.Services.Api;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Dashboard.Controllers;
+
+public class CategoriesController(CategoryService categoryService) : Controller
+{
+    // GET
+    public async Task<IActionResult> Index(int page = 1, string? searchTerm = null)
+    {
+        var categoryResult = await categoryService.GetCategories(searchTerm, new PaginationQuery(20, page));
+        if (!categoryResult.IsSuccess)
+            return RedirectToAction("Login", "Account");
+
+        var categories = categoryResult.Value;
+
+        var viewModel = new CategoryListViewModel
+        {
+            Categories = categories.ToList(),
+            SearchTerm = searchTerm
+        };
+
+        return View(viewModel);
+    }
+}
---
+++ ECommerce.Dashboard/Models/Categories/CategoryListViewModel.cs
@@ -0,0 +1,9 @@
+﻿using ECommerce.Dashboard.DTOs.Category;
+
+namespace ECommerce.Dashboard.Models.Categories;
+
+public class CategoryListViewModel
+{
+    public List<CategoryResponse> Categories { get; set; } = new();
+    public string? SearchTerm { get; set; }
+}
