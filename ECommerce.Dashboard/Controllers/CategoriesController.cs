804bc6bb095196cee86bcd7608d75e4a2fd5e103
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-05-05 20:51:02 -0400

    Add remaining actions to CategoriesController.cs

--- ECommerce.Dashboard/Controllers/CategoriesController.cs
+++ ECommerce.Dashboard/Controllers/CategoriesController.cs
@@ -1,5 +1,9 @@
-﻿using ECommerce.Dashboard.DTOs.Shared;
+﻿using System.Net;
+using ECommerce.Dashboard.DTOs.Category;
+using ECommerce.Dashboard.DTOs.Shared;
 using ECommerce.Dashboard.Models.Categories;
+using ECommerce.Dashboard.Models.Products;
+using ECommerce.Dashboard.Results;
 using ECommerce.Dashboard.Services.Api;
 using Microsoft.AspNetCore.Mvc;
 
@@ -23,5 +27,126 @@ public class CategoriesController(CategoryService categoryService) : Controller
         };
 
         return View(viewModel);
+    }
+
+    public IActionResult Create()
+    {
+        return View(new CategoryCreateViewModel());
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(CategoryCreateViewModel model)
+    {
+        if (!ModelState.IsValid)
+            return View(model);
+
+        var request = new CategoryRequest
+        {
+            Name = model.Name,
+            Slug = model.Slug
+        };
+        
+        var result = await categoryService.CreateCategory(request);
+        if (result.IsSuccess)
+        {
+            TempData["Success"] = "Category created successfully";
+            return RedirectToAction(nameof(Index));
+        }
+
+        if (result.Error is ApiTokensError)
+            return RedirectToAction("Login", "Account");
+
+        var responseError = result.Error as ApiResponseError;
+        var errorMessage = responseError?.ErrorBody?.Message ?? throw new InvalidOperationException();
+        TempData["Error"] = errorMessage;
+
+        return View(model);
+    }
+
+    public async Task<IActionResult> Edit(int id)
+    {
+        var categoryResult = await categoryService.GetCategory(id);
+        if (!categoryResult.IsSuccess)
+            return RedirectToAction("Login", "Account");
+
+        var category = categoryResult.Value;
+
+        var viewModel = new CategoryUpdateViewModel
+        {
+            Id = category.Id,
+            Name = category.Name,
+            Slug = category.Slug
+        };
+
+        return View(viewModel);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, CategoryUpdateViewModel model)
+    {
+        if (!ModelState.IsValid)
+            return View(model);
+
+        var request = new CategoryRequest
+        {
+            Name = model.Name,
+            Slug = model.Slug
+        };
+        
+        var result = await categoryService.UpdateCategory(id, request);
+        if (result.IsSuccess)
+        {
+            TempData["Success"] = "Category updated successfully";
+            return RedirectToAction(nameof(Index));
+        }
+
+        if (result.Error is ApiTokensError)
+            return RedirectToAction("Login", "Account");
+
+        var responseError = result.Error as ApiResponseError;
+        if (responseError?.StatusCode == HttpStatusCode.NotFound)
+            return RedirectToAction(nameof(NotFound));
+        
+        var errorMessage = responseError?.ErrorBody?.Message ?? throw new InvalidOperationException();
+        TempData["Error"] = errorMessage;
+
+        return View(model);
+    }
+    
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var result = await categoryService.DeleteCategory(id);
+        if (!result.IsSuccess)
+        {
+            TempData["Error"] = "Failed to delete product";
+        }
+        else
+        {
+            TempData["Success"] = "Product deleted successfully";
+        }
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    // public async Task<IActionResult> Details(int id)
+    // {
+    //     var result = await categoryService.GetCategoryByIdOrSlug(id.ToString());
+    //     if (result.IsSuccess)
+    //         return View(result.Value);
+    //
+    //     if (result.Error is ApiTokensError)
+    //         return RedirectToAction("Login", "Account");
+    //
+    //     return RedirectToAction(nameof(NotFound));
+    // }
+
+    [ActionName("NotFound")]
+    public IActionResult ProductNotFound()
+    {
+        return View();
     }
 }
