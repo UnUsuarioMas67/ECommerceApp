532b739b973505c5ed6550edb4be8c3a17c80fce
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-05-05 20:29:13 -0400

    Add CategoryCreateViewModel.cs and CategoryUpdateViewModel.cs

---
+++ ECommerce.Dashboard/Models/Categories/CategoryCreateViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce.Dashboard.Models.Categories;
+
+public class CategoryCreateViewModel
+{
+    [Required]
+    [StringLength(50)]
+    public string Slug { get; set; } = string.Empty;
+    
+    [Required]
+    [StringLength(50)]
+    public string Name { get; set; } = string.Empty;
+}
---
+++ ECommerce.Dashboard/Models/Categories/CategoryUpdateViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce.Dashboard.Models.Categories;
+
+public class CategoryUpdateViewModel
+{
+    [Required]
+    public int Id { get; set; }
+    
+    [Required]
+    [StringLength(50)]
+    public string Slug { get; set; } = string.Empty;
+    
+    [Required]
+    [StringLength(50)]
+    public string Name { get; set; } = string.Empty;
+}
