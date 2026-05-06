fd476ee0a17e8c43abcad67903029442b7087168
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-05-05 23:03:51 -0400

    Add ClientService.cs

---
+++ ECommerce.Dashboard/Services/Api/ClientService.cs
@@ -0,0 +1,52 @@
+﻿using System.Net;
+using System.Text;
+using ECommerce.Dashboard.DTOs.Shared;
+using ECommerce.Dashboard.DTOs.User;
+using ECommerce.Dashboard.Results;
+
+namespace ECommerce.Dashboard.Services.Api;
+
+public class ClientService(ApiRequestService apiRequestService)
+{
+    private const string ClientsPath = "api/clients";
+
+    public async Task<Result<UserResponse>> GetClientById(int id)
+    {
+        var options = new ApiRequestOptions
+        {
+            Path = ClientsPath + "/" + id,
+            Method = HttpMethod.Get,
+            ExpectedFailCodes = [HttpStatusCode.NotFound]
+        };
+
+        return await apiRequestService.SendAsync<UserResponse>(options);
+    }
+
+    public async Task<Result<IEnumerable<UserResponse>>> GetClients(
+        string? search = null,
+        PaginationQuery? paginationQuery = null)
+    {
+        var query = new StringBuilder($"?search={search ?? ""}");
+        if (paginationQuery != null)
+            query.Append($"&limit={paginationQuery.Limit}&page={paginationQuery.Page}");
+
+        var options = new ApiRequestOptions
+        {
+            Path = ClientsPath + query,
+            Method = HttpMethod.Get,
+        };
+
+        return await apiRequestService.SendAsync<IEnumerable<UserResponse>>(options);
+    }
+
+    public async Task<Result<UserResponse>> GetAuthClient()
+    {
+        var options = new ApiRequestOptions
+        {
+            Path = ClientsPath + "/me",
+            Method = HttpMethod.Get,
+        };
+
+        return await apiRequestService.SendAsync<UserResponse>(options);
+    }
+}
