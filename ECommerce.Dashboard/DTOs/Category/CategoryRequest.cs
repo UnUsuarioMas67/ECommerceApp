afd459eaf2bff0d89c1ca16026dd252530169274
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-05-05 20:46:41 -0400

    Update CategoryRequest.cs

--- ECommerce.Dashboard/DTOs/Category/CategoryRequest.cs
+++ ECommerce.Dashboard/DTOs/Category/CategoryRequest.cs
@@ -4,11 +4,6 @@ namespace ECommerce.Dashboard.DTOs.Category;
 
 public class CategoryRequest
 {
-    [Required]
-    [StringLength(50)]
-    public string Slug { get; set; } = string.Empty;
-    
-    [Required]
-    [StringLength(50)]
-    public string Name { get; set; } = string.Empty;
+    public required string Slug { get; set; } = string.Empty;
+    public required string Name { get; set; } = string.Empty;
 }
