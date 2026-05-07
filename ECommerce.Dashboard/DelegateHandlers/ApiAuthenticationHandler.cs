4cd4e963dbe2014e5d3fd71afb1015c953ad7f72
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-05-06 20:48:26 -0400

    Add ApiAuthenticationHandler.cs
    This class is used to handle the authorization header when sending API requests.

---
+++ ECommerce.Dashboard/DelegateHandlers/ApiAuthenticationHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using System.Net.Http.Headers;
+using ECommerce.Dashboard.Services;
+
+namespace ECommerce.Dashboard.DelegateHandlers;
+
+public class ApiAuthenticationHandler(AuthService authService) : DelegatingHandler
+{
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        var accessToken = await authService.GetAccessTokenAsync();
+        if (accessToken == null)
+            return new HttpResponseMessage(HttpStatusCode.Unauthorized);
+        
+        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+        var response = await base.SendAsync(request, cancellationToken);
+        
+        return response;
+    }
+}
