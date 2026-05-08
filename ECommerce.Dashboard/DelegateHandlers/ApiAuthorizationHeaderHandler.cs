91b8b206a58ad59092db7d8e8e7f3730ccb26514
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-05-08 19:54:29 -0400

    Update HTTP message handler
    Renamed ApiAuthenticationHandler to ApiAuthorizationHeaderHandler. The class no longer handles token refreshes and simply retrieves the access token and passes it to the request headers.

---
+++ ECommerce.Dashboard/DelegateHandlers/ApiAuthorizationHeaderHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http.Headers;
+using ECommerce.Dashboard.Settings;
+using Microsoft.Extensions.Options;
+
+namespace ECommerce.Dashboard.DelegateHandlers;
+
+public class ApiAuthorizationHeaderHandler(IOptions<AuthSettings> options, IHttpContextAccessor httpContextAccessor)
+    : DelegatingHandler
+{
+    private readonly AuthSettings _authSettings = options.Value;
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+        CancellationToken cancellationToken)
+    {
+        var httpContext = httpContextAccessor.HttpContext;
+        if (httpContext?.Request.Cookies.ContainsKey(_authSettings.JwtCookieKey) == true)
+        {
+            var accessToken = httpContext.Request.Cookies[_authSettings.JwtCookieKey];
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+        }
+
+        var response = await base.SendAsync(request, cancellationToken);
+
+        return response;
+    }
+}
