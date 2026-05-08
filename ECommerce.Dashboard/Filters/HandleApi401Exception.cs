ca9c9c0d184e80de220db08b30e4f4887296a645
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-05-08 19:57:03 -0400

    Add HandleApi401Exception.cs
    This adds a filter that will return the user to the login page when an API call fails and throws an exception with a 401 response status code.

---
+++ ECommerce.Dashboard/Filters/HandleApi401Exception.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ECommerce.Dashboard.Filters;
+
+public class HandleApi401Exception : IAsyncExceptionFilter
+{
+    public async Task OnExceptionAsync(ExceptionContext context)
+    {
+        var httpRequestException = context.Exception as HttpRequestException;
+        if (httpRequestException == null)
+            return;
+
+        if (httpRequestException.StatusCode != HttpStatusCode.Unauthorized)
+            return;
+
+        await context.HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        context.Result = new RedirectToActionResult("Login", "Account", null);
+    }
+}
