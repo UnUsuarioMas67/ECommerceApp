edf7b6e25e8341b86d2f3febe4179ab68dc17df9
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-05-11 17:39:55 -0400

    Add UserService.cs

---
+++ ECommerce.Dashboard/Services/Api/UserService.cs
@@ -0,0 +1,34 @@
+﻿using ECommerce.Dashboard.DTOs;
+using ECommerce.Dashboard.DTOs.User;
+using ECommerce.Dashboard.Models.Auth;
+using ECommerce.Dashboard.Results;
+
+namespace ECommerce.Dashboard.Services.Api;
+
+public class UserService(ApiRequestService apiRequestService)
+{
+    private const string AdminPath = "api/admins";
+    
+    public async Task<AdminUser> GetCurrent()
+    {
+        var options = new ApiRequestOptions
+        {
+            Path = AdminPath + "/me",
+            Method = HttpMethod.Get,
+        };
+        
+        return await apiRequestService.SendAlwaysSucceedAsync<AdminUser>(options);
+    }
+
+    public async Task<Result<AdminUser>> UpdateUser(AdminUserUpdate request)
+    {
+        var options = new ApiRequestOptions
+        {
+            Path = AdminPath,
+            Method = HttpMethod.Put,
+            Body = request
+        };
+        
+        return await apiRequestService.SendAsync<AdminUser>(options);
+    }
+}
