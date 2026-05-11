528078e804497d8ae7d3c6d4bdc8e49bbc8970e2
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-05-11 17:39:47 -0400

    Add AdminUserUpdate.cs

---
+++ ECommerce.Dashboard/DTOs/User/AdminUserUpdate.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerce.Dashboard.DTOs.User;
+
+public class AdminUserUpdate
+{
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string Password { get; set; }
+    public required string PhoneNumber { get; set; }
+    public required string BirthDate { get; set; }
+}
