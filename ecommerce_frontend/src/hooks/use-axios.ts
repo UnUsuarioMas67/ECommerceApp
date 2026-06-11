2fe8bd12ff4458282e0a8e19175226cb7034e6ae
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-06-10 20:53:24 -0400

    Add use-axios custom hook

---
+++ ecommerce_frontend/src/hooks/use-axios.ts
@@ -0,0 +1,33 @@
+import axios, { type InternalAxiosRequestConfig } from 'axios';
+import { useAuth } from '../components/auth/context/AuthContext';
+import { useMemo } from 'react';
+
+export type ApiRequestError = {
+  type: 'failure_response' | 'request_failed' | 'not_found' | 'unauthorized' | 'server_error' | 'aborted';
+  message?: string;
+};
+
+type ExternalAxiosRequestConfig = InternalAxiosRequestConfig & { _retry?: boolean };
+
+export function useAxios() {
+  const context = useAuth();
+
+  const axiosInstance = useMemo(() => {
+    const instance = axios.create({
+      baseURL: 'http://localhost:5113/api',
+    });
+
+    instance.interceptors.request.use(async (config: ExternalAxiosRequestConfig) => {
+      console.log('request interceptor in use');
+      const accessToken = await context.getAccessToken();
+      if (accessToken) {
+        config.headers.Authorization = `Bearer ${accessToken}`;
+      }
+      return config;
+    });
+
+    return instance;
+  }, [context]);
+
+  return axiosInstance;
+}
