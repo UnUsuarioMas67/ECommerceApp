d109aa0e2bdc55eb8b67ae7cc03144f5c65fd2d4
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-06-11 03:43:53 -0400

    Add placeholder main page

--- ecommerce_frontend/src/main.tsx
+++ ecommerce_frontend/src/main.tsx
@@ -1,16 +1,28 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './themes/Spacelab/bootstrap.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './themes/Spacelab/bootstrap.min.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, Routes, Route } from 'react-router';
-import HomeLayout from './layout/HomeLayout.tsx';
+import AuthProvider from './components/auth/context/AuthProvider.tsx';
+import MainLayout from './layout/MainLayout.tsx';
+import NotFoundLayout from './layout/NotFoundLayout.tsx';
+import LoginForm from './components/auth/LoginForm.tsx';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomeLayout />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<MainLayout />} />
+            <Route path="/login" element={<LoginForm />} />
+            <Route path="*" element={<NotFoundLayout />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>,
 );
