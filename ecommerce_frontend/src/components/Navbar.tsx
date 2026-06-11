e8811c8333182566af35a58c9b3966788d0fa08e
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-06-11 03:40:27 -0400

    Add Navbar.tsx
    Placeholder navbar component.

---
+++ ecommerce_frontend/src/components/Navbar.tsx
@@ -0,0 +1,37 @@
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Button from 'react-bootstrap/Button';
+
+function NavbarComponent() {
+  return (
+    <Navbar expand="lg" className="bg-dark mb-4" data-bs-theme="dark">
+      <Container>
+        <Navbar.Brand href="#home">ECommerce</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+          <Form>
+            <InputGroup>
+              <Form.Control
+                placeholder="Search..."
+                aria-label="Search"
+                aria-describedby="basic-addon2"
+              />
+              <Button variant="primary" id="button-addon2" type='submit'>
+                Search
+              </Button>
+            </InputGroup>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavbarComponent;
