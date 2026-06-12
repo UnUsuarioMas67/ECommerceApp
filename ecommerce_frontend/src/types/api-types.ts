119c09b0b3aecdf03b5686cf27f59f7c51f8e1f1
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-06-11 22:01:48 -0400

    Update api-types.ts

--- ecommerce_frontend/src/types/api-types.ts
+++ ecommerce_frontend/src/types/api-types.ts
@@ -32,3 +32,58 @@ export type Category = {
   slug: string;
   name: string;
 };
+
+export type Address = {
+  id: number;
+  clientId: number;
+  addressLine1: string;
+  addressLine2: string;
+  country: string;
+  region: string;
+  city: string;
+  postalCode: string;
+};
+
+export type CartItem = {
+  product: Product;
+  quantity: number;
+};
+
+export type Cart = {
+  id: number;
+  clientId: number;
+  items: CartItem[];
+  totalPrice: number;
+  totalProducts: number;
+};
+
+export type OrderItem = {
+  productId: number;
+  productName: string;
+  productSku: string;
+  quantity: number;
+  unitPrice: number;
+  subtotal: number;
+};
+
+export type Order = {
+  id: number;
+  clientId: number | null;
+  clientEmail: string | null;
+  clientName: string | null;
+  address: Address;
+  orderDate: string;
+  items: OrderItem;
+  status: string;
+  totalPrice: number;
+  totalProducts: number;
+};
+
+export type CheckoutSession = {
+  sessionId: string;
+  url: string;
+  amount: number;
+  currency: string;
+  expiresAt: string;
+  order: Order;
+};
