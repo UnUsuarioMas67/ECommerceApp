baa6af1f9a3c5d6c041e85fc05baafb6a08e1fb8
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-06-16 03:55:46 -0400

    Update antiforgery endpoint to return json object instead of raw string

--- ECommerce.Api/Program.cs
+++ ECommerce.Api/Program.cs
@@ -64,8 +64,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseStaticFiles();
 app.UseHttpsRedirection();
-app.MapGet("/antiforgery",
-        (IAntiforgery antiforgery, HttpContext httpContext) => antiforgery.GetAndStoreTokens(httpContext).RequestToken)
+app.MapGet("/api/antiforgery", (IAntiforgery antiforgery, HttpContext httpContext) 
+            => new { antiforgery.GetAndStoreTokens(httpContext).RequestToken })
     .RequireAuthorization(UserRoles.Admin)
     .WithTags("Antiforgery Token")
     .WithSummary("Get Antiforgery token");
