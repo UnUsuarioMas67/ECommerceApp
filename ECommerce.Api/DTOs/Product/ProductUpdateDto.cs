7cf4ca3a8ec62f79308f26028996ea4c829a6ad4
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-06-16 02:04:56 -0400

    Update ProductCreateDto.cs and ProductUpdateDto.cs
    Added properties for image file and antiforgery token.

--- ECommerce.Api/DTOs/Product/ProductUpdateDto.cs
+++ ECommerce.Api/DTOs/Product/ProductUpdateDto.cs
@@ -3,10 +3,11 @@
 public class ProductUpdateDto
 {
     public string? Category { get; set; }
-    
+
     public string? Name { get; set; }
     public string? Description { get; set; }
     public decimal? Price { get; set; }
-    
     public string? ImageUrl { get; set; }
+    public IFormFile? ImageFile { get; set; }
+    public string __RequestVerificationToken { get; set; }
 }
