07ef56e9fb322621a37005cca39acfe09dd56baa
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-06-21 17:40:27 -0400

    Add router context

--- ecommerce_frontend/src/main.tsx
+++ ecommerce_frontend/src/main.tsx
@@ -1,29 +1,52 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './themes/Cerulean/bootstrap.css';
-import { StrictMode } from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import AuthProvider from './components/auth/context/AuthProvider';
 
 // Import the generated route tree
-import { routeTree } from './routeTree.gen'
+import { routeTree } from './routeTree.gen';
+import { useAuth } from './components/auth/context/AuthContext';
+import { useAxios } from './hooks/use-axios';
+
+const queryClient = new QueryClient();
 
 // Create a new router instance
-const router = createRouter({ routeTree })
+const router = createRouter({
+  routeTree,
+  context: {
+    queryClient: undefined!,
+    authContext: undefined!,
+    axiosInstance: undefined!
+  },
+});
 
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
   interface Register {
-    router: typeof router
+    router: typeof router;
   }
 }
 
 // Render the app
-const rootElement = document.getElementById('root')!
+const rootElement = document.getElementById('root')!;
 if (!rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement)
+  const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </QueryClientProvider>
     </StrictMode>,
-  )
+  );
+}
+
+function App() {
+  const authContext = useAuth();
+  const axiosInstance = useAxios();
+  return <RouterProvider router={router} context={{ queryClient, authContext, axiosInstance }} />;
 }
--- ecommerce_frontend/src/routes/__root.tsx
+++ ecommerce_frontend/src/routes/__root.tsx
@@ -1,23 +1,24 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
+import { createRootRouteWithContext, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
-import AuthProvider from '../components/auth/context/AuthProvider';
 import NotFoundLayout from '../layout/NotFoundLayout';
+import type { QueryClient } from '@tanstack/react-query';
+import type { useAuth } from '../components/auth/context/AuthContext';
+import type { AxiosInstance } from 'axios';
 
-const queryClient = new QueryClient();
+type RouterContext = {
+  queryClient: QueryClient;
+  authContext: ReturnType<typeof useAuth>
+  axiosInstance: AxiosInstance
+}
 
 const RootLayout = () => (
   <>
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <Outlet />
-      </AuthProvider>
-    </QueryClientProvider>
+    <Outlet />
     <TanStackRouterDevtools />
   </>
 );
 
-export const Route = createRootRoute({
-  component: RootLayout, 
-  notFoundComponent: NotFoundLayout
+export const Route = createRootRouteWithContext<RouterContext>()({
+  component: RootLayout,
+  notFoundComponent: NotFoundLayout,
 });
