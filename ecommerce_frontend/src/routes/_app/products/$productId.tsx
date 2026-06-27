3d16ee7159c94e4e15f29dea67a4aeb347dbb71c
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-06-26 21:51:54 -0400

    Update product view route

--- ecommerce_frontend/src/routes/_app/products/$productId.tsx
+++ ecommerce_frontend/src/routes/_app/products/$productId.tsx
@@ -1,31 +1,77 @@
-import { createFileRoute } from '@tanstack/react-router';
-import type { AxiosInstance } from 'axios';
+import { createFileRoute, Navigate, notFound } from '@tanstack/react-router';
 import { useAxios } from '../../../hooks/use-axios';
-import type { Product } from '../../../api/types';
-import { useQuery } from '@tanstack/react-query';
-import ProductCard from '../../../components/ProductCard';
-import { imagesUrl } from '../../../api';
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { fetchProduct } from '../../../api';
+import { isAxiosError } from 'axios';
+import Col from 'react-bootstrap/esm/Col';
+import Row from 'react-bootstrap/esm/Row';
+import ProductImage from '../../../components/ProductImage';
+import AddToCartButton from '../../../components/AddToCartButton';
 
 export const Route = createFileRoute('/_app/products/$productId')({
   component: RouteComponent,
-});
 
-async function fetchProduct(id: string, axios: AxiosInstance) {
-  const response = await axios.get<Product>(`/products/${id}`);
-  return response.data;
-}
+  loader: ({ params: { productId }, context: { axiosInstance, queryClient } }) => {
+    queryClient.ensureQueryData({
+      queryKey: ['products', productId],
+      queryFn: () =>
+        fetchProduct(axiosInstance, Number(productId)).catch((error) => {
+          if (isAxiosError(error)) {
+            if (error.response?.status === 404) throw notFound();
+          }
+        }),
+    });
+  },
+  notFoundComponent: () => <Navigate to="/" />,
+  errorComponent: ErrorComponent,
+});
 
 function RouteComponent() {
   const axiosInstance = useAxios();
   const { productId } = Route.useParams();
-  const { data, isLoading, isError } = useQuery({
+  const { data: product } = useSuspenseQuery({
     queryKey: ['products', productId],
-    queryFn: () => fetchProduct(productId, axiosInstance),
+    queryFn: () => fetchProduct(axiosInstance, Number(productId)),
   });
 
-  if (isLoading) return <h1>Loading</h1>;
+  return (
+    <>
+      <Row className="g-5 mb-5 justify-content-center">
+        <Col xs={12} md={6} xl={5}>
+          <ProductImage
+            className="border rounded-3"
+            productSrc={product.imageUrl ?? ''}
+            fallback="https://placehold.co/640"
+            fluid
+          />
+        </Col>
+        <Col xs={12} md={6} xl={5}>
+          <h1>{product.name}</h1>
+          <p className="text-secondary">{product.sku}</p>
 
-  if (isError) return <h1 className="text-danger">Error</h1>;
+          <p className="fs-3 fw-bold">${product.price}</p>
+          <hr />
 
-  return data && <ProductCard imagesUrl={imagesUrl} product={data} />;
+          <div className="d-flex justify-content-center justify-content-md-start">
+            <AddToCartButton product={product} />
+          </div>
+        </Col>
+        <Col xs={12} xl={10}>
+          <h3>Description</h3>
+          <p>{product.description}</p>
+        </Col>
+      </Row>
+    </>
+  );
+}
+
+function ErrorComponent() {
+  return (
+    <Row className="pt-5">
+      <Col className="text-center">
+        <h1>Something went wrong!</h1>
+        <p className="fs-4">An unexpected error has ocurred.</p>
+      </Col>
+    </Row>
+  );
 }
