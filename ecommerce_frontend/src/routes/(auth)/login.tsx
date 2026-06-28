0aff29dcf34aec345cb5476a7fdd56467e452e39
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-06-28 01:06:24 -0400

    Improve login page responsivity

--- ecommerce_frontend/src/routes/(auth)/login.tsx
+++ ecommerce_frontend/src/routes/(auth)/login.tsx
@@ -1,13 +1,6 @@
-import { createFileRoute, Link, useNavigate } from '@tanstack/react-router';
-import { useForm, type SubmitHandler } from 'react-hook-form';
-import { loginSchema, type LoginRequest } from '../../schemas';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useLogin } from '../../hooks/account';
-
-import Container from 'react-bootstrap/esm/Container';
+import { createFileRoute, Link } from '@tanstack/react-router';
 import Row from 'react-bootstrap/esm/Row';
 import Col from 'react-bootstrap/esm/Col';
-import Card from 'react-bootstrap/esm/Card';
 import LoginForm from './-login-form';
 
 export const Route = createFileRoute('/(auth)/login')({
@@ -18,12 +11,15 @@ function RouteComponent() {
   return (
     <>
       <Row className="justify-content-center flex-grow-1 g-0 align-items-stretch" as="main">
-        <Col lg={8} className='bg-primary text-center pt-5'>
+        <Col lg={8} className='bg-primary text-center pt-5 d-none d-lg-block'>
           <Link className='h2 text-light text-decoration-none' to="/">
             ECommerce
           </Link>
         </Col>
-        <Col className='p-5 d-flex flex-column justify-content-center border-start'>
+        <Col className='p-5 d-flex flex-column justify-content-start justify-content-lg-center border-start'>
+          <Link className='h2 text-center text-decoration-none d-lg-none' to="/" style={{marginBottom: '100px'}}>
+            ECommerce
+          </Link>
           <h1 className="text-center text-body mb-5">Sign Up</h1>
           <LoginForm />
           <span className='text-center small'>Don't have an account? <Link to='/register'>Register</Link></span>
