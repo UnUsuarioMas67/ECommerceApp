5c8adb823ac442d6dbdb5dc9103a16169562d1b5
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-06-30 04:06:42 -0400

    Update CartProvider
    Modify setItems function so that when an item with a quantity of 0 or lower is passed it will remove the product from the list if it exists.

--- ecommerce_frontend/src/components/CartProvider/index.tsx
+++ ecommerce_frontend/src/components/CartProvider/index.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useState } from 'react';
 import { CartContext } from './CartContext';
-import type { CartItem, Product } from '../../api/types';
+import type { CartItem } from '../../api/types';
 
 type Props = {
   children: React.ReactNode;
@@ -25,29 +25,32 @@ function CartProvider({ children }: Props) {
 
   const getItem = useCallback((id: number) => items.find((item) => item.product.id === id), [items]);
 
+  const removeItem = useCallback((id: number) => {
+    setItems((prev) => prev.filter((ci) => ci.product.id !== id));
+  }, []);
+
   const setItem = useCallback(
     (item: CartItem) => {
       const {
         product: { id },
+        quantity,
       } = item;
 
       if (getItem(id)) {
-        setItems((prev) =>
-          prev.map((i) => {
-            if (id === i.product.id) return validItem(item);
-            else return i;
-          }),
-        );
-      } else {
+        if (quantity > 0)
+          setItems((prev) =>
+            prev.map((i) => {
+              if (id === i.product.id) return validItem(item);
+              else return i;
+            }),
+          );
+        else removeItem(id);
+      } else if (quantity > 0) {
         setItems((prev) => [...prev, validItem(item)]);
       }
     },
-    [getItem, validItem],
+    [getItem, validItem, removeItem],
   );
-
-  const removeItem = useCallback((id: number) => {
-    setItems((prev) => prev.filter((ci) => ci.product.id !== id));
-  }, []);
 
   const clearCart = useCallback(() => {
     setItems([]);
