22d581b574c9cca6dfe00e1df091c6bfd5471dbc
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-07-01 19:52:52 -0400

    Update AddToCartButton to use CartContext to manage it's state

--- ecommerce_frontend/src/components/AddToCartButton.tsx
+++ ecommerce_frontend/src/components/AddToCartButton.tsx
@@ -1,9 +1,8 @@
-import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import type { Product } from '../api/types';
 import { Plus, Dash, Trash } from 'react-bootstrap-icons';
 import InputGroup from 'react-bootstrap/InputGroup';
-import Form from 'react-bootstrap/Form';
+import { useCart } from './CartProvider/CartContext';
 
 type Props = {
   product: Product;
@@ -11,67 +10,33 @@ type Props = {
 };
 
 function AddToCartButton({ product, disabled }: Props) {
-  // TODO: Create a CartProvider to manage the shopping cart
+  const { getItemQuantity, increaseItemQuantity, decreaseItemQuantity } = useCart();
 
-  const [mode, setMode] = useState<'button' | 'counter'>('button');
-  const [quantity, setQuantity] = useState(0);
+  const style = { maxWidth: '144px', width: '100%' };
+  const quantity = getItemQuantity(product.id);
 
-  const style = { maxWidth: '144px', width: '144px' };
-
-  const updateQuantity = (value: number) => {
-    let n = value;
-
-    if (n <= 0)
-    {
-      n = 0;
-      setMode('button')
-    } else if (n > product.stock) {
-      n = product.stock
-    }
-    
-    setQuantity(n)
-  }
-
-  const onBtnClick = () => {
-    setQuantity(1);
-    setMode('counter');
-  };
-
-  const onDeleteClick = () => {
-    updateQuantity(0)
-  }
-
-  const onInputChange = (value: string) => {
-    const n = value === '' ? 0 : parseInt(value);
-    if (!isNaN(n)) updateQuantity(n);
-  };
-
-  return mode === 'button' ? (
-    <Button disabled={product.stock == 0 || disabled} variant="primary" onClick={onBtnClick} style={style}>
+  return quantity <= 0 ? (
+    <Button
+      disabled={product.stock == 0 || disabled}
+      variant="primary"
+      onClick={() => increaseItemQuantity(product.id)}
+      style={style}>
       {product.stock > 0 ? 'Add to cart' : 'Out of stock'}
     </Button>
   ) : (
-    <InputGroup style={style}>
-      {quantity > 1 ? (
-        <Button variant="primary" onClick={() => updateQuantity(quantity - 1)}>
-          <Dash />
+    <>
+      <InputGroup style={style}>
+        <Button variant="primary" onClick={() => decreaseItemQuantity(product.id)}>
+          {quantity > 1 ? <Dash /> : <Trash />}
         </Button>
-      ) : (
-        <Button variant="primary" onClick={onDeleteClick}>
-          <Trash />
+
+        <InputGroup.Text id="basic-addon2" className='flex-sm-grow-1 justify-content-center'>{getItemQuantity(product.id)}</InputGroup.Text>
+
+        <Button variant="primary" onClick={() => increaseItemQuantity(product.id)} disabled={product.stock <= quantity}>
+          <Plus />
         </Button>
-      )}
-
-      <Form.Control
-        className="bg-secondary text-center"
-        value={quantity}
-        onChange={(e) => onInputChange(e.target.value)}
-      />
-
-      <Button variant="primary" onClick={() => updateQuantity(quantity + 1)} disabled={product.stock <= quantity}>
-        <Plus />
-      </Button>
-    </InputGroup>
+      </InputGroup>
+    </>
   );
 }
 
