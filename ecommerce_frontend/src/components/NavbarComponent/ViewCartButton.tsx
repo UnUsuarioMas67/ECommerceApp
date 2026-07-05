8ce7376d5cd2e466c3ccc1fbb0886cc20a0f5c8d
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-07-04 22:39:46 -0400

    Add badge to display items in cart in ViewCartButton component

--- ecommerce_frontend/src/components/NavbarComponent/ViewCartButton.tsx
+++ ecommerce_frontend/src/components/NavbarComponent/ViewCartButton.tsx
@@ -1,13 +1,20 @@
 import { Cart4 } from 'react-bootstrap-icons';
 import Nav from 'react-bootstrap/esm/Nav';
+import Badge from 'react-bootstrap/esm/Badge';
 import { useCart } from '../CartProvider/CartContext';
 
 function ViewCartButton() {
-  const {openCart, totalItems} = useCart()
+  const { openCart, totalItems } = useCart();
 
   return (
-    <Nav.Link className="ms-2" onClick={openCart} disabled={totalItems === 0}>
-      <Cart4 size={24} title="View cart" /> {totalItems > 0 && <span className='small'>{totalItems}</span>}
+    <Nav.Link className="ms-2 position-relative" onClick={openCart} disabled={totalItems === 0}>
+      <Cart4 size={24} title="View cart" />
+      {totalItems > 0 && (
+        <Badge className='position-absolute top-0 start-100 translate-middle' bg="dark" pill>
+          {totalItems}
+        </Badge>
+      )}
+      <span className="visually-hidden">items in cart</span>
     </Nav.Link>
   );
 }
