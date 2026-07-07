21a7eed8a0107670eb48cae3364d3b17a14fa143
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-07-06 20:12:00 -0400

    Add fetchCurrentUser function to api/user.ts

--- ecommerce_frontend/src/api/user.ts
+++ ecommerce_frontend/src/api/user.ts
@@ -1,6 +1,6 @@
-import type { AxiosInstance } from "axios";
-import type { LoginRequest, RegisterRequest } from "../schemas";
-import type { UserAuth, User } from "./types";
+import type { AxiosInstance } from 'axios';
+import type { LoginRequest, RegisterRequest } from '../schemas';
+import type { UserAuth, User } from './types';
 
 export async function postLogin(axiosInstance: AxiosInstance, data: LoginRequest) {
   const response = await axiosInstance.post<UserAuth>('/clients/login', data);
@@ -13,5 +13,10 @@ export async function postLogout(axiosInstance: AxiosInstance) {
 
 export async function postRegister(axiosInstance: AxiosInstance, data: RegisterRequest) {
   const response = await axiosInstance.post<User>('/clients/register', data);
+  return response.data;
+}
+
+export async function fetchCurrentUser(axiosInstance: AxiosInstance) {
+  const response = await axiosInstance.get<User>('/clients/me');
   return response.data;
 }
