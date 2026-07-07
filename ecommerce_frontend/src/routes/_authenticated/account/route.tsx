e557a54e8a449ac2ddf367c4f9ffb8e88d60bfc3
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-07-07 00:21:34 -0400

    Create layout with sidebar for _authenticated/account routes

--- ecommerce_frontend/src/routes/_authenticated/account/route.tsx
+++ ecommerce_frontend/src/routes/_authenticated/account/route.tsx
@@ -1,9 +1,40 @@
-import { createFileRoute, Outlet } from '@tanstack/react-router';
+import { createFileRoute, Link, Outlet } from '@tanstack/react-router';
+import { CartCheck, GeoAlt, Gear } from 'react-bootstrap-icons';
+import Col from 'react-bootstrap/esm/Col';
+import Container from 'react-bootstrap/esm/Container';
+import Nav from 'react-bootstrap/esm/Nav';
+import Row from 'react-bootstrap/esm/Row';
 
 export const Route = createFileRoute('/_authenticated/account')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <Outlet />;
+  return (
+    <Container className='flex-grow-1'>
+      <Row className='h-100'>
+        <Col xs={3} as="aside">
+          <Nav defaultActiveKey="/home" className="flex-column" variant="underline">
+            <Link className="nav-link text-decoration-none d-inline-flex align-items-center" to="/account/orders">
+              <CartCheck size={18} className="me-3" />
+              Your orders
+            </Link>
+
+            <Link className="nav-link text-decoration-none d-inline-flex align-items-center" to="/account/addresses">
+              <GeoAlt size={18} className="me-3" />
+              Your addresses
+            </Link>
+
+            <Link className="nav-link text-decoration-none d-inline-flex align-items-center" to="/account/settings">
+              <Gear size={18} className="me-3" />
+              Account settings
+            </Link>
+          </Nav>
+        </Col>
+        <Col as="main">
+          <Outlet />
+        </Col>
+      </Row>
+    </Container>
+  );
 }
