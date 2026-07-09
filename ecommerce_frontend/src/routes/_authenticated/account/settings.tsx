2810eb3f873571e021810042a756b5633c8a6a84
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-07-09 04:10:19 -0400

    Add account settings route with user data and password update forms

--- ecommerce_frontend/src/routes/_authenticated/account/settings.tsx
+++ ecommerce_frontend/src/routes/_authenticated/account/settings.tsx
@@ -1,9 +1,52 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router';
+import { fetchCurrentUser } from '../../../api/user';
+import { useQueryClient, useSuspenseQuery } from '@tanstack/react-query';
+import { useAxios } from '../../../hooks/use-axios';
+import Container from 'react-bootstrap/esm/Container';
+import Row from 'react-bootstrap/esm/Row';
+import UpdateUserDataForm from '../../../components/AccountSettings/UpdateUserDataForm';
+import LoadingSpinner from '../../../components/LoadingSpinner';
+import type { User } from '../../../api/types';
+import UpdateUserPasswordForm from '../../../components/AccountSettings/UpdateUserPasswordForm';
 
 export const Route = createFileRoute('/_authenticated/account/settings')({
   component: RouteComponent,
-})
+  pendingComponent: LoadingSpinner,
+  loader: ({ context: { axiosInstance, queryClient } }) => {
+    queryClient.prefetchQuery({
+      queryKey: ['users', 'me'],
+      queryFn: () => fetchCurrentUser(axiosInstance),
+    });
+  },
+});
 
 function RouteComponent() {
-  return <div>Hello "/_authenticated/account/settings"!</div>
+  const queryClient = useQueryClient();
+  const axiosInstance = useAxios();
+  const { data: user } = useSuspenseQuery({
+    queryKey: ['users', 'me'],
+    queryFn: () => fetchCurrentUser(axiosInstance),
+  });
+
+  const onUpdateUserDataSubmitSuccessful = (newUser: User) => {
+    queryClient.setQueryData<User>(['users', 'me'], newUser);
+  };
+
+  return (
+    <Container className='mb-4'>
+      <h1 className="mb-4">Account settings</h1>
+
+      <Row>
+        <h3>User data</h3>
+        <UpdateUserDataForm user={user} onSubmitSuccessful={onUpdateUserDataSubmitSuccessful} />
+      </Row>
+
+      <hr className="my-5" />
+
+      <Row>
+        <h3>Password</h3>
+        <UpdateUserPasswordForm />
+      </Row>
+    </Container>
+  );
 }
