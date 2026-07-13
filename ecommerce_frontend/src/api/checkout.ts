745434a692905f0a14b04920827f6c6a9201d2c2
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2026-07-13 01:56:53 -0400

    Update api/checkout.ts to introduce some error handling

--- ecommerce_frontend/src/api/checkout.ts
+++ ecommerce_frontend/src/api/checkout.ts
@@ -1,7 +1,52 @@
-import type { AxiosInstance } from 'axios';
+import { isAxiosError, type AxiosInstance } from 'axios';
 import type { CheckoutRequest } from '../schemas/checkout';
+import type { Api422ErrorBody } from './types';
 
 export async function createCheckoutSession(axiosInstance: AxiosInstance, data: CheckoutRequest) {
-  const response = await axiosInstance.post('/checkout/session', { ...data, successUrl: 'http://localhost:5173', cancelUrl: '' });
-  return response.data;
+  try {
+    const response = await axiosInstance.post('/checkout/session', {
+      ...data,
+      successUrl: 'http://localhost:5173',
+      cancelUrl: '',
+    });
+    return response.data;
+  } catch (error) {
+    if (isAxiosError(error)) {
+
+      if (error.response?.status === 422) {
+        const errorObj = error.response.data as Api422ErrorBody;
+
+        if (errorObj.errorType === 'insufficient_stock') {
+          const details = errorObj.details as ProductsStockErrorDetails;
+          throw new ProductsStockError(details.products)
+        }
+      }
+    }
+
+    throw error;
+  }
+}
+
+export function isProductsStockError(obj: unknown) {
+  return obj instanceof ProductsStockError
+}
+
+export type ProductsStockErrorItem = {
+  productId: number;
+  quantityRequested: number;
+  stockAvailable: number;
+};
+
+type ProductsStockErrorDetails = {
+  products: ProductsStockErrorItem[];
+};
+
+export class ProductsStockError extends Error implements ProductsStockErrorDetails {
+  products: ProductsStockErrorItem[];
+
+  constructor(products: ProductsStockErrorItem[]) {
+    super('One or more items have insufficient stock');
+    this.name = 'ProductsStockError';
+    this.products = products;
+  }
 }
